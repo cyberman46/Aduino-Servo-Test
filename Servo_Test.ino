e4250be42432ad5bc935e5d6395ab3375b765e74
cyberman46 <akekapon27@gmail.com>
2017-05-05 11:07:40 +0700

    delete comment

--- Servo_Test.ino
+++ Servo_Test.ino
@@ -3,17 +3,13 @@ Servo myServo;
 
 void setup() {
   myServo.attach(9); 
-  //myServo.write(0);
-  //delay(0);
 }
 
 void loop() {
-
-  //myServo.write(0);                       // ส่งค่าไปควบคุม servo เป็นมุมที่ต้องการในช่วง 0-179 องศา
-  //delay(1000); 
-  myServo.write(90);                       // ส่งค่าไปควบคุม servo เป็นมุมที่ต้องการในช่วง 0-179 องศา
+  
+  myServo.write(90);                     
   delay(500);
-  myServo.write(0);                       // ส่งค่าไปควบคุม servo เป็นมุมที่ต้องการในช่วง 0-179 องศา
+  myServo.write(0);
   delay(1000);                 
 
 }
